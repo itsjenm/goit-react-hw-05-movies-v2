03ed5fe72c9296f15cdf761d9e684a4c4e8b55ce
Jenny <77757004+itsjenm@users.noreply.github.com>
2023-06-18 04:37:16 -0500

    fixed console errors

--- src/components/movie-search/Movies.jsx
+++ src/components/movie-search/Movies.jsx
@@ -1,27 +1,24 @@
 import React from 'react';
-import { useEffect } from 'react';
-import axios from 'axios';
-import { useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 
 const Movies = () => {
-  const [movieID, setMovieID] = useState();
-  const [search, setSearch] = useState('');
+  // const [movieID, setMovieID] = useState();
+  // const [search, setSearch] = useState('');
   const [searchParam, setSearchParam] = useSearchParams();
   const title = searchParam.get('title') || '';
   // const [data, setData] = useState(() => )
 
   console.log(title);
   const nav = useNavigate();
-  useEffect(() => {
-    axios
-      .get(
-        `https://api.themoviedb.org/3/movie/550?api_key=36aae46d872dddaed5df56cf6ef9643b`
-      )
-      .then(response => {
-        console.log(response.data);
-      });
-  });
+  // useEffect(() => {
+  //   axios
+  //     .get(
+  //       `https://api.themoviedb.org/3/movie/550?api_key=36aae46d872dddaed5df56cf6ef9643b`
+  //     )
+  //     .then(response => {
+  //       console.log(response.data);
+  //     });
+  // });
 
   function handleInputChange(event) {
     setSearchParam({ title: event.target.value });
