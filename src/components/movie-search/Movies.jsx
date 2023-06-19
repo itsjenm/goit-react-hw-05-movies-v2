bf0b6a7b35b221674448fdbc83e1e30b4e147558
Jenny <77757004+itsjenm@users.noreply.github.com>
2023-06-19 00:48:23 -0500

    styling fixes

--- src/components/movie-search/Movies.jsx
+++ src/components/movie-search/Movies.jsx
@@ -1,6 +1,7 @@
 import fetchMovies from 'api/fetchMovies';
 import React, { useEffect, useState, useRef } from 'react';
 import { Link, useNavigate, useSearchParams } from 'react-router-dom';
+import Styled from './Movies.module.css'
 
 const Movies = () => {
   // const [movieID, setMovieID] = useState();
@@ -50,14 +51,13 @@ const Movies = () => {
   }
 
   return (
-    <div>
+    <div className={Styled.form_container}>
       <form onSubmit={handleSubmit}>
-        <h1>Movies</h1>
-        <label>Enter a title you want to search for</label>
-        <input type="text" onChange={handleInputChange} value={formValues.name} ref={searchRef} />
-        <button type="submit">Submit</button>
+        <h1 className={Styled.form_title}>Movies</h1>
+        <input type="text" onChange={handleInputChange} value={formValues.name} ref={searchRef} placeholder='Enter a title you want to search for' className={Styled.inputbox}/>
+        <button type="submit">Search</button>
         {results.length > 0 ? (
-          <ul>
+          <ul className={Styled.movie_list}>
             {results.map(movie => (
               <li key={movie.id}>
                 <h3><Link to={`/movies/${movie.id}`}>{movie.original_title}</Link></h3>
